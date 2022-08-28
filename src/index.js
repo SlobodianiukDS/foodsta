80c53c33af5dd4302defcc86c21229d346cb028d
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-08-28 12:05:28 +0300

    подключаю слайдер

---
+++ src/index.js
@@ -0,0 +1,9 @@
+<script
+  type="text/javascript"
+  src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
+></script>;
+
+<script
+  type="text/javascript"
+  src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"
+></script>;
