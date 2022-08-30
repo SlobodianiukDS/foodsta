0ddb7621be47108f7576f68ef38c3e9d56c44753
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-08-30 17:11:13 +0300

    исправления

--- src/js/mod.js
+++ src/js/mod.js
@@ -1,26 +1,20 @@
 (() => {
   const refs = {
-    openModalBtnHead: document.querySelector(
-      '[data-modal-open1]',
-    ),
-openModalBtnHero: document.querySelector(
-      '[data-modal-open2]',
-    ),
-openModalBtnOffer: document.querySelector(
-      '[data-modal-open3]'
-    ),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('[data-page]'),
+    openModalBtnHead: document.querySelector("[data-modal-open1]"),
+    openModalBtnHero: document.querySelector("[data-modal-open2]"),
+    openModalBtnOffer: document.querySelector("[data-modal-open3]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("[data-page]"),
   };
 
-  refs.openModalBtnHead.addEventListener('click', toggleModal);
-refs.openModalBtnHero.addEventListener('click', toggleModal);
-refs.openModalBtnOffer.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnHead.addEventListener("click", toggleModal);
+  refs.openModalBtnHero.addEventListener("click", toggleModal);
+  refs.openModalBtnOffer.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
